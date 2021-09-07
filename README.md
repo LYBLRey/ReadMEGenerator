10c3aebec46f0da9448e783b3537722ef08b17e2
Sareybodd Chhorn <sareyboddc@gmail.com>
2021-09-06 23:37:37 -0700

    readme properly generated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+# Test123
+​
+motivation was to be better. Built this project to learn. Problem this solves is figuring out how to template a readme. I learned how to use js better
+​
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions](#questions)​
+​
+## Installation
+npm i fs inquirer
+​
+## Usage
+helps generate a readme
+​
+## Contribution
+shoutout to caffeine filled tea and cold showers to help keep me awake and focused to work on assignments
+​
+## Tests
+test with node index.js
+​
+## Questions 
+[Github](https://github.com/lyblrey/)
 
+[Email](mailto:testemail@email.com)
--- index.js
+++ index.js
@@ -37,44 +37,44 @@ inquirer
     {
       type: "input",
       name: "title",
-      message: "\n\nWhat is the Project Name\n\n",
+      message: "\nName of Project title:\n",
     },
     {
       type: "input",
       name: "description",
       message:
-        '\n\nDescription\nProvide a short description explaining the what, why, and how of your project.\nUse the following questions as a guide.\n- What was your motivation?\n- Why did you build this project?\n(Note: the answer is not "Because it was a homework assignment.")\n- What problem does it solve? \n- What did you learn?\n\n',
+        '\nDescription\nProvide a short description explaining the what, why, and how of your project.\nUse the following questions as a guide.\n- What was your motivation?\n- Why did you build this project?\n(Note: the answer is not "Because it was a homework assignment.")\n- What problem does it solve? \n- What did you learn?\n',
     },
     {
       type: "input",
       name: "installation",
       message:
-        "\n\nInstallation\nWhat are the steps required to install your project? \nProvide a step-by-step description of how to get the development environment running.\n\n",
+        "\nInstallation\nWhat are the steps required to install your project? \nProvide a step-by-step description of how to get the development environment running.\n",
     },
     {
       type: "input",
       name: "usage",
       message:
-        "\n\nUsage\nProvide instructions and examples for use. Include screenshots as needed.\nTo add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it.\nThen, using the relative filepath, add it to your README using the following syntax:\n\n\n ```md \n![alt text](assets/images/screenshot.png) \n```\n\n",
+        "\nUsage\nProvide instructions and examples for use. Include screenshots as needed.\nTo add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it.\nThen, using the relative filepath, add it to your README using the following syntax:\n\n\n ```md \n![alt text](assets/images/screenshot.png) \n```\n",
     },
     {
       type: "input",
       name: "contribution",
       message:
-        "\n\nContribution\nList your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution,\nlist the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.\n\n",
+        "\nContribution\nList your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution,\nlist the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.\n",
     },
     {
       type: "input",
       name: "tests",
       message:
-        "\nTests\nGo the extra mile and write tests for your application.\n Then provide examples on how to run them here.\n\n",
+        "\nTests\nGo the extra mile and write tests for your application.\n Then provide examples on how to run them here.\n",
     },
     {
       type: "list",
       name: "license",
       message:
-        "\nLicense\n\nThe last section of a high-quality README file is the license.\nThis lets other developers know what they can and cannot do with your project.\nIf you need help choosing a license, refer to https://choosealicense.com/.\n",
-      choices: ["APACHE 2.0", "MIT"],
+        "\nLicense\n\nThe last section of a high-quality README file is the license.\nThis lets other developers know what they can and cannot do with your project.\nIf you need help choosing a license, refer to https://choosealicense.com/ \n",
+      choices: ["APACHE 2.0", "MIT", "Mozilla Public", "GNU LGPLv3"],
     },
     {
       type: "input",
@@ -93,4 +93,5 @@ inquirer
       path.join(__dirname, "README.md"),
       generateMarkdown(answers)
     );
+    console.log("README GENERATED YAY!");
   });
