f61d3bad6498005e15b879b57d2ff99273df3aa3
Sareybodd Chhorn <sareyboddc@gmail.com>
2021-09-06 23:13:18 -0700

    tested code and generates readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ReadMEGenerator
+
--- index.js
+++ index.js
@@ -1,23 +1,96 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-const generateMarkdown = require("generateMarkdown");
+const path = require("path");
 
-const questions = () => {
-  return inquirer.prompt([
-    { type: "input", name: "title", message: "Title of the README:" },
-  ]);
+const generateMarkdown = (data) => {
+  return `# ${data.title}
+​
+${data.description}
+​
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions](#questions)​
+​
+## Installation
+${data.installation}
+​
+## Usage
+${data.usage}
+​
+## Contribution
+${data.contribution}
+​
+## Tests
+${data.tests}
+​
+## Questions 
+[Github](https://github.com/${data.github}/)\n
+[Email](mailto:${data.email})
+`;
 };
 
-const writeFile = util.promisify(fs.writeFile);
-
-function init() {
-  questions()
-    .then((answers) => {
-      writeFile("./GeneratedREADME/README.md", generateMarkdown(answers));
-    })
-    .then(() => console.log("README GENERATED YAY"))
-    .catch((err) => console.error(err));
-}
-
-init();
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "\n\nWhat is the Project Name\n\n",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        '\n\nDescription\nProvide a short description explaining the what, why, and how of your project.\nUse the following questions as a guide.\n- What was your motivation?\n- Why did you build this project?\n(Note: the answer is not "Because it was a homework assignment.")\n- What problem does it solve? \n- What did you learn?\n\n',
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "\n\nInstallation\nWhat are the steps required to install your project? \nProvide a step-by-step description of how to get the development environment running.\n\n",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "\n\nUsage\nProvide instructions and examples for use. Include screenshots as needed.\nTo add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it.\nThen, using the relative filepath, add it to your README using the following syntax:\n\n\n ```md \n![alt text](assets/images/screenshot.png) \n```\n\n",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message:
+        "\n\nContribution\nList your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution,\nlist the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.\n\n",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "\nTests\nGo the extra mile and write tests for your application.\n Then provide examples on how to run them here.\n\n",
+    },
+    {
+      type: "list",
+      name: "license",
+      message:
+        "\nLicense\n\nThe last section of a high-quality README file is the license.\nThis lets other developers know what they can and cannot do with your project.\nIf you need help choosing a license, refer to https://choosealicense.com/.\n",
+      choices: ["APACHE 2.0", "MIT"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "\nEnter your GitHub Username\n\n",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "\nEnter your Email address.\n\n",
+    },
+  ])
+  .then((answers) => {
+    console.log("answers:", answers);
+    fs.writeFileSync(
+      path.join(__dirname, "README.md"),
+      generateMarkdown(answers)
+    );
+  });
